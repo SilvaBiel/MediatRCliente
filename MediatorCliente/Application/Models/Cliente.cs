59bf0446e27ba0abe2e567159194f7a81b299f41
Gabriel Santos <bielusantos@ucl.br>
2023-06-07 10:09:12 -0300

    Adicionar arquivos de projeto.

---
+++ MediatorCliente/Application/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿namespace MediatorCliente.Application.Models
+{
+    using System.ComponentModel.DataAnnotations;
+        public class Cliente
+        {
+
+            [Key]
+            public int ClienteId { get; set; }
+            public string ClienteName { get; set; } = string.Empty;
+            public string ClienteLastName { get; set; } = string.Empty;
+            public long ClientePhone { get; set; }
+            public string ClienteEmail { get; set; } = string.Empty;
+            public string ClienteCep { get; set; } = string.Empty;
+            public long ClienteCPF { get; set; }
+        }
+
+}
